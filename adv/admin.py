ec585fb3b80531c2bfeb105eefa62b4176b95391
kiteretsu-daihyakka <61159866+kiteretsu-daihyakka@users.noreply.github.com>
2020-03-02 22:23:40 +0530

    Add files via upload

---
+++ adv/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Country, State, City, Area
+
+
+
+
+
+
+
+
+
+#from .models import Publisher
+# Register your models here.
+#admin.site.register(Publisher)
+# admin.site.register(Country)
+# admin.site.register(State)
+# admin.site.register(City)
+# admin.site.register(Area)
